efaf70f78da91b184b3bb401f9e918bb52613786
Adi <adelina_yordanova11@abv.bg>
2016-05-16 07:16:26 +0300

    Homework3_14.

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+	public static void main(String[] args) {
+		int n = 5;
+		int k = 3;
+
+		long factorialn = 1;
+		long factorialk = 1;
+		for (int i = 1; i <= n; i++) {
+			factorialn *= i;
+		}
+		for (int j = 1; j <= k; j++) {
+			factorialk *= j;
+		}
+
+		System.out.println(n + "! = " + factorialn);
+		System.out.println(k + "! = " + factorialk);
+
+		int a = 1;
+
+		if (a < k && k < n) {
+
+			System.out.println("N!/K! =" + (factorialn / factorialk));
+		}
+
+	}
+}
